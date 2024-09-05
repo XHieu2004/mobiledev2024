a5560d016885c5cba30eb567cc298d3e2251e4a7
unknown <hieutx.22bi13163@usth.edu.vn>
2024-09-06 00:27:19 +0700

    Practical Work 5

---
+++ Weather_App/app/src/main/java/com/example/weather_app/WeatherActivity.java
@@ -0,0 +1,63 @@
+package com.example.weather_app;
+
+import android.os.Bundle;
+
+
+import android.util.Log;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class WeatherActivity extends AppCompatActivity {
+
+    private static final String TAG = "WeatherActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+
+
+        if (savedInstanceState == null) {
+            ForecastFragment forecastFragment = new ForecastFragment();
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, forecastFragment)
+                    .commit();
+        }
+    }
+
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart() called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume() called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause() called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop() called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy() called");
+    }
+}
