6ebe547221a3a9f3ae20536e934cd971c4d5ceaf
KD616 <hieutx.22bi13163@usth.edu.vn>
2024-09-13 10:24:52 +0700

    Practical Work 7

--- Weather_App/app/src/main/java/com/example/weather_app/WeatherAndForecastFragment.java
+++ Weather_App/app/src/main/java/com/example/weather_app/WeatherAndForecastFragment.java
@@ -1,41 +1,39 @@
 package com.example.weather_app;
 
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-
-
 public class WeatherAndForecastFragment extends Fragment {
 
+    private WeatherFragment weatherFragment;
+    private ForecastFragment forecastFragment;
+
     public WeatherAndForecastFragment() {
-        // Required empty public constructor
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_weather_and_forecast, container, false);
 
+
+        weatherFragment = new WeatherFragment();
+        forecastFragment = new ForecastFragment();
+
+
         FragmentManager fragmentManager = getChildFragmentManager();
-        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
+        fragmentTransaction.add(R.id.weather_fragment_container, weatherFragment);
+        fragmentTransaction.add(R.id.forecast_fragment_container, forecastFragment);
 
-        transaction.replace(R.id.weather_fragment_container, new WeatherFragment());
-
-
-        transaction.replace(R.id.forecast_fragment_container, new ForecastFragment());
-
-        transaction.commit();
-        Log.d("WeatherAndForecastFragment", "Fragments added");
+        fragmentTransaction.commit();
 
         return view;
     }
 }
-
